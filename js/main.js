3ffbe914eae41140c86ce347bae6ec1c99135d34
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-06-09 13:30:44 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+var cardOne = "Queen";
+var cardTwo = "King";
+var cardThree = "King";
+var cardFour = "Queen";
+
+console.log("User flipped " + cardOne);
+
+console.log("User flipped " + cardTwo);
