ece6427d3ab9ee2a4d45117c744e6570baf9357f
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-06-13 13:39:51 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
+var cards = ['queen', 'queen', 'king', 'king'];
 
-var cardOne = "Queen";
-var cardTwo = "King";
-var cardThree = "King";
-var cardFour = "Queen";
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-console.log("User flipped " + cardTwo);
+
+cardsInPlay.push(cardOne);
+
+console.log('user flipped '+cardOne);
+
+cardsInPlay.push(cardTwo);
+
+console.log('user flipped '+cardTwo);
+
+// Have 2 cards been played
+if (cardsInPlay.length === 2){
+
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert('You found a match');
+	}
+	else {
+		alert('Sorry, try again');
+	}
+}
