4827a792fb43e039ad42a9e9f23d95a19db08599
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-06-19 11:51:52 -0700

    created game logic with new functions

--- js/main.js
+++ js/main.js
@@ -2,25 +2,27 @@ var cards = ['queen', 'queen', 'king', 'king'];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-
-cardsInPlay.push(cardOne);
-
-console.log('user flipped '+cardOne);
-
-cardsInPlay.push(cardTwo);
-
-console.log('user flipped '+cardTwo);
-
+var checkForMatch = function() {
 // Have 2 cards been played
-if (cardsInPlay.length === 2){
+	if (cardsInPlay.length === 2){
 
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert('You found a match');
-	}
-	else {
-		alert('Sorry, try again');
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert('You found a match');
+		}
+		else {
+			alert('Sorry, try again');
+		}
 	}
 }
+
+var flipCard = function(cardId) {
+	
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
